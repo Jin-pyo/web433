38489690ca85d2eb8d704d4dc2fdac6c913dc39a
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2020-01-06 18:08:55 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app = express();
+const cors=require("cors");
+const bodyParser = require("body-parser");
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+let names = [{Name:"pat",lName:"C"},{Name:"john",lName:"smith"}]
+
+app.get("/api/names",(req,res)=>{
+    res.json(names);
+});
+
+app.listen(HTTP_PORT,()=>{
+    console.log(`serverlog ${HTTP_PORT}`);
+});
