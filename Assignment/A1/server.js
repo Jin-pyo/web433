a39cc7123becd2ee53dc30f1cbaca032fa41addb
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2020-01-13 18:53:30 -0500

    assignment1 try

--- Assignment/A1/server.js
+++ Assignment/A1/server.js
@@ -16,11 +16,20 @@ const HTTP_PORT = process.env.PORT || 8080;
 // ************* API Routes
 
 // POST /api/sales (NOTE: This route must read the contents of the request body)
+app.post("/api/sales",(req,res)=>{
+    myData.addNewSale(req.body)
+    .then((msg)=>{
+        res.json(msg);
+    })
+    .catch((err)=>{
+        res.json({message:`error ${err}`});
+    })
 
+})
 
 // GET /api/sales (NOTE: This route must accept the numeric query parameters "page" and "perPage", ie: /api/sales?page=1&perPage=5 )
 app.get("/api/sales",(req,res)=>{
-    myData.getAllSales(1,5).then((data)=>{
+    myData.getAllSales(req.query.page,req.query.perPage).then((data)=>{
         res.json(data);
     })
     .catch((err)=>{
@@ -38,7 +47,15 @@ app.get("/api/sales",(req,res)=>{
 
 
 // DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
-
+app.delete("/api.sales/:id",(req,res)=>{
+    myData.deleteSaleById(req.params.id)
+    .then((msg)=>{
+        res.json({message:msg});
+    })
+    .catch((err)=>{
+        
+    })
+})
 
 // ************* Initialize the Service & Start the Server
 
