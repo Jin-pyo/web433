d24664743652838697ed3615be4e9ce90c6b0977
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2020-01-17 13:04:42 -0500

    w2
    during lab class

--- w2/js/main.js
+++ w2/js/main.js
@@ -1,10 +1,20 @@
 $(function(){
-    console.log("DOM is ready!");
+    
+    $("#data-table tbody tr.highlight").each(function(index){
+        console.log(index);
+        $(this).css({"background-color":"#eff6f7" });
+    });
+
+    $("#data-table tbody").on("click","tr")
 
     $("#table-one tbody").on("click","tr",function(){
         console.log("table row clicked");
     });
 
+    $("#table-one tbody").append(`<tr>
+                                
     
+                            </tr>`)
 
 });
+
