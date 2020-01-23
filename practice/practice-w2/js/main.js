2be4ca555ce9aa08764816441c31f51f9781b1df
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2020-01-23 00:38:40 -0500

    summary

--- practice/practice-w2/js/main.js
+++ practice/practice-w2/js/main.js
@@ -1,5 +1,5 @@
-
 $(function(){
+
     
     $("#data-table tbody tr.highlight").each(function(index){
         console.log(index);
@@ -7,28 +7,27 @@ $(function(){
     });
 
 
-    $("#data-table tbody").on("click","tr",function(){
+    $("#data-table tbody").on("click","td",function(){
        console.log($(this).attr("_id"));
     })
 
     $("#data-table tbody").append(`
-        <tr _id="4">
-            <td>Ten</td>
-            <td>Eleven</td>
-            <td>Twelve</td>
+        <tr >
+            <td _id="4">Ten</td>
+            <td _id="4">Eleven</td>
+            <td _id="4">Twelve</td>
         </tr>
     `);
 
     /*
+
     $("#data-table tbody tr td").html(`
         <span>New div!</span>
     `)
 
     */
 
-   $("#data-form").on("submit",function(e){
-    e.preventDefault();
-
+  
     
     
 
@@ -47,33 +46,10 @@ $(function(){
             //radio
             vehicle:$("#data-form [name='vehicle']:checked").val()
         }
-
         console.log(formData);
-    })
-    
-})
-   
+
+        })
 
 
-/*
+});    
 
-    let formData={
-        //text
-        firstName: $("#data-form [name='fName']").val(),
-        //textarea
-        bio:$("#data-form [name='bio']").val(),
-        //select
-        food:$("#data-form [name='food']").val(),
-        //checkbox
-        enrolled:$("#data-form [name='enrolled']").prop("checked"),
-        //radio
-        vehicle:$("#data-form [name='vehicle']:checked").val()
-    }
-   
-    $("#data-table tbody tr td").append(`
-        <span>${formData.firstName}</span>
-    `)
-
-    console.log(formData);
-})
-*/
